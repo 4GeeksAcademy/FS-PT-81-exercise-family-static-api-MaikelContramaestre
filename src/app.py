c5f00922ef4ad5a0ffcf60bdd30019b6f219c4d4
Maikel Contramaestre <116273585+BlackNereus@users.noreply.github.com>
2024-12-10 08:11:42 +0000

    exercise family API

--- src/app.py
+++ src/app.py
@@ -15,6 +15,14 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    'name':'juanito',
+    'age': 34,
+    'id': 34,
+    'lucky_number' :[34]
+})
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -35,8 +43,37 @@ def handle_hello():
         "family": members
     }
 
+    return jsonify(response_body), 200
+@app.route('/members', methods=['POST'])
+def handle_add():
+    data = request.json
+    if 'name' not in data or 'age' not in data or 'lucky_number' not in data:
+        return jsonify({'msg': 'Faltan datos por agregar'}), 400    
+    response = jackson_family.add_member(data)
+    print (data)
+    return jsonify({'msg': response}), 200
+
+@app.route('/members/<int:member_id>' , methods=['DELETE'])
+def handle_delete(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        jackson_family.delete_member(member_id)
+        return jsonify({"message": f"Usuario con ID {member_id} eliminado."}), 200
+    else:
+         return jsonify({"error": "Usuario no encontrado"}), 404
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def handle_find(member_id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_member(member_id)
+    response_body = {
+        
+        "family": members
+    }
 
     return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
